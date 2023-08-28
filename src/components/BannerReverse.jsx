d890f9a74416c583e5c9237865b787ff19b865d7
Darshan <darshanpatel2923@gmail.com>
2023-08-28 18:51:32 +0530

    Added the layouts for the whole banner section.

--- src/components/BannerReverse.jsx
+++ src/components/BannerReverse.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import "../styles/Banner.css";
 
-const BannerReverse = () => {
+function BannerReverse({ title, text, img }) {
     return (
-        <div>BannerReverse</div>
-    )
+        <div className="banner">
+            <div className="container">
+                <div className="banner-container-reverse">
+                    <div className="text-side">
+                        <div className="text">
+                            <h2>{title}</h2>
+                            <p>{text}</p>
+                            <Link onClick={() => window.scrollTo(0, 0)} to="categories/all">
+                                <button>Shop now</button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="img-side">
+                        <img src={img} alt="banner" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default BannerReverse
+export default BannerReverse;
