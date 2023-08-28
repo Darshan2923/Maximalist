d3d9b7bdf180775c89f25684792b5347421bbf08
Darshan <darshanpatel2923@gmail.com>
2023-08-28 18:29:21 +0530

    Added footer to our project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Newsletter from '../components/NewsLetter'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Newsletter />
+            <Footer />
         </>
     )
 }
