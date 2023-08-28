decb45f03ee8555f43af1222389d65272fcf20a9
Darshan <darshanpatel2923@gmail.com>
2023-08-28 19:47:41 +0530

    Started with the layout of another section.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ import Banner1 from '../assets/banner/banner2.png'
 import Banner2 from '../assets/banner/banner3.png'
 import Banner from '../components/Banner'
 import BannerReverse from '../components/BannerReverse'
+import ProudProducts from '../components/ProudProducts'
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <ProudProducts />
             <Banner
                 title="Youthful College Accessories"
                 text=" Our Products are all made to standard sizes so that you can mix and match them freely."
