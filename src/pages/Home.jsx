ac27ecc81d1038d5f50c278af52ec34e757e73c4
Darshan <darshanpatel2923@gmail.com>
2023-08-28 18:48:58 +0530

    Added layout for banners onto the main project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Newsletter from '../components/NewsLetter'
 import Footer from '../components/Footer'
+import Banner1 from '../assets/banner/banner2.jpg'
+import Banner2 from '../assets/banner/banner3.jpg'
+import Banner from '../components/Banner'
+import BannerReverse from '../components/BannerReverse'
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <Banner
+                title="Creative harmonious living"
+                text=" RAOUF Products are all made to standard sizes so that you can mix and match them freely."
+                img={Banner1}
+            />
+            <BannerReverse
+                title="Comfortable & Elegante Living"
+                text=" RAOUF Products are all made to standard sizes so that you can mix and match them freely."
+                img={Banner2}
+            />
             <Newsletter />
             <Footer />
         </>
