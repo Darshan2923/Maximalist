54c56b5777b35f2294418bbe3f7b82917508410f
Darshan <darshanpatel2923@gmail.com>
2023-08-29 10:51:52 +0530

    Added ProductData section and images

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { items } from './FullData';
+import { Link } from 'react-router-dom';
+import '../styles/ProudProducts.css'
 
 const ProductItem = () => {
+    const filteredItems = items.filter((item) => item.id <= 8);
+
     return (
-        <div>ProductItem</div>
+        <>
+            {filteredItems.map((item) => (
+                <div className="product normal" key={item.id}>
+                    <Link
+                        onClick={() => window.top(0, 0)}
+                        to={`/categories/product/${item.id}`}>
+                        <div className="product-header">
+                            <img src={item.img} alt="product1" />
+                        </div>
+                        <div className="product-details">
+                            <p>{item.description}</p>
+                            <p className="item-price">{item.price}₹</p>
+                        </div>
+                    </Link>
+                </div>
+            ))}
+        </>
     )
 }
 
