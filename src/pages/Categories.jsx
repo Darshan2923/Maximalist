9d202f6c31c3b8aeef0749950edee1130fbc37cc
Darshan <darshanpatel2923@gmail.com>
2023-08-29 15:01:57 +0530

    Started with adding newsletter and footer to page

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import CategoriesHeader from '../components/CategoriesHeader'
+import Newsletter from '../components/NewsLetter'
+import Footer from '../components/Footer'
+import CategoriesItem from '../components/CategoriesItem'
 
 const Categories = () => {
     return (
         <>
             <CategoriesHeader />
+            <CategoriesItem />
+            <Newsletter />
+            <Footer />
         </>
     )
 }
