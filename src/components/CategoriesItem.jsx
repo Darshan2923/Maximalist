3db64fd8a0e8df31239b75e83bf30e80c8d979b0
Darshan <darshanpatel2923@gmail.com>
2023-08-29 15:11:30 +0530

    Did a little change in flow of categories page

--- src/components/CategoriesItem.jsx
+++ src/components/CategoriesItem.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { items } from './FullData'
+import { Link } from 'react-router-dom'
 
 const CategoriesItem = () => {
     return (
-        <div>CategoriesItem</div>
+        <>
+            <div className="proud-container">
+                <div className="container">
+                    <div className="products-grid">
+                        {items.map((item) => (
+                            <div key={item.id} className="product normal">
+                                <Link
+                                    onClick={() => window.top(0, 0)}
+                                    to={`/categories/product/${item.id}`}
+                                >
+                                    <div className="product-header">
+                                        <img src={item.img} alt="product1" />
+                                    </div>
+                                    <div className="product-details">
+                                        <p>{item.description}</p>
+                                        <p className="item-price">{item.price}$</p>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import CategoriesHeader from '../components/CategoriesHeader'
-import Newsletter from '../components/NewsLetter'
-import Footer from '../components/Footer'
-import CategoriesItem from '../components/CategoriesItem'
+import { Outlet } from 'react-router-dom'
 
 const Categories = () => {
     return (
         <>
             <CategoriesHeader />
-            <CategoriesItem />
-            <Newsletter />
-            <Footer />
+            <Outlet />
         </>
     )
 }
