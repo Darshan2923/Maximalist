6d1bcb26ce9ea4cb8cc59a2517f4218e55b71535
Darshan <darshanpatel2923@gmail.com>
2023-08-29 10:52:42 +0530

    Added half of the data required for the project

--- src/components/FullData.jsx
+++ src/components/FullData.jsx
@@ -1,4 +1,103 @@
 import React from 'react'
+import Img1 from '../assets/products/img1.jpg'
+import Img2 from '../assets/products/img2.jpg'
+import Img3 from '../assets/products/img3.jpg'
+import Img4 from '../assets/products/img4.jpg'
+import Img5 from '../assets/products/img5.jpg'
+import Img6 from '../assets/products/img6.jpg'
+import Img7 from '../assets/products/img7.jpg'
+import Img8 from '../assets/products/img8.jpg'
+import Img9 from '../assets/products/img9.jpg'
+import Img10 from '../assets/products/img10.jpg'
+import Img11 from '../assets/products/img11.jpg'
+import Img12 from '../assets/products/img12.jpg'
+import Img13 from '../assets/products/img13.jpg'
+import Img14 from '../assets/products/img14.jpg'
+import Img15 from '../assets/products/img15.jpg'
+import Img16 from '../assets/products/img16.jpg'
+import Img17 from '../assets/products/img17.jpg'
+import Img18 from '../assets/products/img18.jpg'
+import Img19 from '../assets/products/img19.jpg'
+import Img20 from '../assets/products/img20.jpg'
+
 export const items = [
+    {
+        id: 1,
+        category: 'womens',
+        img: Img1,
+        description: "Rain Jacket Women Windbreaker",
+        price: 1199,
+        specs: "Lightweight perfect for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+        texture: "Cotton",
+        rating: {
+            "rate": 3.8,
+            "count": 679
+        }
+    },
+    {
+        id: 2,
+        category: 'womens',
+        img: Img2,
+        description: "Women Biker Jacket",
+        price: 1499,
+        specs: "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+        texture: "Cotton",
+        "rating": {
+            "rate": 2.9,
+            "count": 340
+        }
+    },
+    {
+        id: 3,
+        category: 'jewellery',
+        img: Img3,
+        description: "Gold Petite Micropave",
+        price: 23999,
+        specs: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+        texture: "Gold",
+        "rating": {
+            "rate": 3.9,
+            "count": 70
+        }
+    },
+    {
+        id: 4,
+        category: 'mens',
+        img: Img4,
+        description: "Mens Cotton Jacket",
+        price: 1499,
+        specs: "Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+        texture: "Cotton",
+        "rating": {
+            "rate": 4.7,
+            "count": 500
+        }
+    },
+    {
+        id: 5,
+        category: 'electronics',
+        img: Img5,
+        description: "Samsung Gaming Monitor",
+        price: 45000,
+        specs: "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag.",
+        texture: "metalloid",
+        "rating": {
+            "rate": 2.2,
+            "count": 140
+        }
+    },
+    {
+        id: 6,
+        category: 'mens',
+        img: Img6,
+        description: "Slim Fit T-Shirts",
+        price: 999,
+        specs: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        texture: "Cotton",
+        "rating": {
+            "rate": 4.1,
+            "count": 259
+        }
+    },
 
 ]
